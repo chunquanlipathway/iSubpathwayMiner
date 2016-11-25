94d53522977adb3774267c4965475a2e80b6a64e
chunquanlipathway <lcqbio@163.com>
2016-11-25 11:18:25 +0800

    1
    1

---
+++ daily.R
@@ -0,0 +1,7 @@
+rm(list=ls())#清空所有数据
+setwd()#更改路径
+read.table(,sep="\t",header=T/F)#tab分隔，第一行作为列名/第一行不作为列名
+colnames()#列名
+rownames()#行名
+write.table(quote=F,row.names=T/F)#输出结果去掉“”，把第一列 作为行名/不作为行名
+install.packages("安装包的名字")#安装R包
